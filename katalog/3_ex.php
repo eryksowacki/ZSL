6bbb9d509ccce71d084d322e016dd91039e68a96
eryksowacki <70707109+eryksowacki@users.noreply.github.com>
2020-09-24 16:40:31 +0200

    Add files via upload

---
+++ katalog/3_ex.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<?php
+  echo '<u> Dane kontaktowe <u>';
+
+  include './nazwisko.php';
+  include './imie.php';
+  echo '<br>include';
+?>
+  </body>
+</html>
