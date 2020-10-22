a0c5eea900e898e4eef6ec965e67e2b524731bdf
eryksowacki <70707109+eryksowacki@users.noreply.github.com>
2020-10-22 19:49:55 +0200

    Add files via upload

---
+++ Mądra_Sowacki/form_user_data.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<?php
+  $konto = ($_POST['Account']);
+  $name = ($_POST['FirstName']);
+  $name = ucwords(strtolower($name));
+  $lastname = ($_POST['LastName']);
+  $lastname = ucwords(strtolower($lastname));
+  $country = ($_POST['CountryRegion']);
+  $adress = ($_POST['StreetAdress']);
+  $postal = ($_POST['ZIPPostalCode']);
+  $code = ($_POST['ZIPPostalCodee']);
+  $city = ($_POST['City']);
+  $state = ($_POST['StateProvince']);
+  $phonenumber = ($_POST['PhoneNumber']);
+  echo<<<DOCC
+  Konto: $konto<br>
+  Imię i nazwisko: $name $lastname.<br>
+  Kraj: $country.<br>
+  Adres 1: $adress<br>
+  DOCC;
+  $secondadress = ($_POST['StreetAdress2']);
+  if(empty($secondadress))
+  {
+    echo " ";
+  }
+  else
+  {
+    echo "Adres 2: $secondadress<br>";
+  }
+echo<<<DOC
+  Kod pocztowy i miasto: $postal-$code, $city".<br>
+  Województwo: $state.<br>
+  Numer telefonu: $phonenumber.<br>
+DOC;
+?>
+
+  </body>
+</html>
